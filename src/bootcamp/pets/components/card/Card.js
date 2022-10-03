a8ad85122e8d582a0ea1de730c25f08d6628106d
raulglezrdguez <raulglezrdguez69@gmail.com>
2022-10-03 09:22:43 -0400

    test and code Card

---
+++ src/bootcamp/pets/components/card/Card.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+// import "./Card.css";
+
+const Card = ({ name, phone, email, image, favorite }) => {
+  const [isFavorite, setIsFavorite] = useState(favorite);
+
+  return (
+    <div className="card">
+      <div className="card-header">
+        <img src={image.url} alt={image.alt} className="card-img" />
+        <button className="heart" onClick={() => setIsFavorite((fav) => !fav)}>
+          <img src="" alt={isFavorite ? "filled heart" : "outline heart"} />
+        </button>
+      </div>
+      <div className="card-content">
+        <p>{phone}</p>
+        <p>{email}</p>
+        <h1>{name}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/bootcamp/pets/components/card/Card.test.js
@@ -0,0 +1,66 @@
+/* eslint-disable testing-library/no-debugging-utils */
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import Card from "./Card";
+
+const cardProps = {
+  name: "Sydney",
+  phone: "1111111111",
+  email: "laura@gmail.com",
+  image: { url: "/images/sydney.png", alt: "sydney" },
+  favorite: false,
+};
+
+describe("card", () => {
+  test("should show props of cat", () => {
+    render(<Card {...cardProps} />);
+
+    // cat name
+    expect(
+      screen.getByRole("heading", { name: /sydney/i })
+    ).toBeInTheDocument();
+
+    // owner phone number
+    expect(screen.getByText(/1111111111/i)).toBeInTheDocument();
+
+    // owner email
+    expect(screen.getByText(/laura@gmail.com/i)).toBeInTheDocument();
+
+    // image cat
+    expect(screen.getByRole("img", { name: /sydney/i })).toBeInTheDocument();
+    // expect(screen.getByAltText(/sydney/i).src).toBe("/images/sydney.png");
+  });
+
+  test("should show outline heart", () => {
+    render(<Card {...cardProps} />);
+
+    expect(screen.getByAltText(/outline heart/i)).toBeInTheDocument();
+    expect(screen.queryByAltText(/filled heart/i)).not.toBeInTheDocument();
+  });
+
+  test("should show filled heart", () => {
+    render(<Card {...cardProps} favorite={true} />);
+
+    expect(screen.getByAltText(/filled heart/i)).toBeInTheDocument();
+    expect(screen.queryByAltText(/outline heart/i)).not.toBeInTheDocument();
+  });
+
+  test("should toggle heart status", () => {
+    render(<Card {...cardProps} />);
+
+    expect(screen.getByAltText(/outline heart/i)).toBeInTheDocument();
+    expect(screen.queryByAltText(/filled heart/i)).not.toBeInTheDocument();
+
+    const heartButton = screen.getByRole("button");
+    userEvent.click(heartButton);
+
+    expect(screen.getByAltText(/filled heart/i)).toBeInTheDocument();
+    expect(screen.queryByAltText(/outline heart/i)).not.toBeInTheDocument();
+
+    userEvent.click(heartButton);
+
+    expect(screen.getByAltText(/outline heart/i)).toBeInTheDocument();
+    expect(screen.queryByAltText(/filled heart/i)).not.toBeInTheDocument();
+  });
+});
