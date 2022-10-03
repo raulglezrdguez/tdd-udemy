ed73c889d2af82ed90d9f3b34e114a1a1afa2f26
raulglezrdguez <raulglezrguez@gmail.com>
2022-10-03 15:58:07 -0400

    test and code Filter component

---
+++ src/bootcamp/pets/components/filter/Filter.js
@@ -0,0 +1,24 @@
+const Filter = () => {
+  return (
+    <div className="pet-filter-container">
+      <div className="filter-container">
+        <label htmlFor="favorite">Favorite</label>
+        <select name="favorite" id="favorite" className="form-select">
+          <option value="any">Any</option>
+          <option value="favorite">Favorite</option>
+          <option value="not favorite">Not favorite</option>
+        </select>
+      </div>
+      <div className="filter-container">
+        <label htmlFor="gender">Gender</label>
+        <select name="gender" id="gender" className="form-select">
+          <option value="any">Any</option>
+          <option value="female">Female</option>
+          <option value="male">Male</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default Filter;
---
+++ src/bootcamp/pets/components/filter/Filter.test.js
@@ -0,0 +1,44 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import Filter from './Filter';
+
+describe('Filter', () => {
+  test('should be able to change value of favorite', () => {
+    render(<Filter />);
+
+    // get favorite select
+    const favoriteSelect = screen.getByLabelText(/favorite/i);
+
+    // initially favorite select set to any
+    expect(favoriteSelect).toHaveValue('any');
+    expect(favoriteSelect.value).toBe('any');
+
+    // select favorite
+    userEvent.selectOptions(favoriteSelect, 'favorite');
+    expect(favoriteSelect).toHaveValue('favorite');
+
+    // select not favorite
+    userEvent.selectOptions(favoriteSelect, 'not favorite');
+    expect(favoriteSelect).toHaveValue('not favorite');
+  });
+
+  test('should be able to change value of gender', () => {
+    render(<Filter />);
+
+    // get gender select
+    const genderSelect = screen.getByLabelText(/gender/i);
+
+    // initially favorite select set to any
+    expect(genderSelect).toHaveValue('any');
+    expect(genderSelect.value).toBe('any');
+
+    // select gender female
+    userEvent.selectOptions(genderSelect, 'male');
+    expect(genderSelect).toHaveValue('male');
+
+    // select gender female
+    userEvent.selectOptions(genderSelect, 'female');
+    expect(genderSelect).toHaveValue('female');
+  });
+});
